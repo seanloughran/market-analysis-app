d36b436806aeba2d33fa53df70f0836151f58060
seanloughran <seancloughran@gmail.com>
2016-04-10 16:25:21 -0700

    renders chart instead of table

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var totalVoteCount = 0;
 document.getElementById('votePlace').innerHTML = totalVoteCount;
 
-var Picture = function(picSource, picNum) {
+var Picture = function(picSource, picNum, label) {
+  this.label = label;
   this.picSource = picSource;
   this.picNum = picNum;
-  this.voteCount = 0;
+  this.y = 0;
   this.addPicture = function(placementID) {
     document.getElementById(placementID).innerHTML = "<img src="+this.picSource+" class='productPicture' id='"+this.picNum+"'>";
     document.getElementById(this.picNum).addEventListener("click", clickPhoto);
@@ -12,22 +13,38 @@ var Picture = function(picSource, picNum) {
   };
 }
 
-var r2bag = new Picture("Images/bag.jpg", 1);
-var banana = new Picture("Images/banana.jpg", 2);
-var boots = new Picture("Images/boots.jpg", 3);
-var chair = new Picture("Images/chair.jpg", 4);
-var monster = new Picture("Images/cthulhu.jpg", 5);
-var dragon = new Picture("Images/dragon.jpg", 6);
-var pen = new Picture("Images/pen.jpg", 7);
-var scissors = new Picture("Images/scissors.jpg", 8);
-var shark = new Picture("Images/shark.jpg", 9);
-var babysweep = new Picture("Images/sweep.jpg", 10);
-var usb = new Picture("Images/usb.jpg", 11);
-var watercan = new Picture("Images/water_can.jpg", 12);
-var wineglass = new Picture("Images/wine_glass.jpg", 13);
-var unicorn = new Picture("Images/unicorn.jpg", 14);
+var r2bag = new Picture("Images/bag.jpg", 1, "R2D2");
+var banana = new Picture("Images/banana.jpg", 2, "Banana");
+var boots = new Picture("Images/boots.jpg", 3, "Boots");
+var chair = new Picture("Images/chair.jpg", 4, "Chair");
+var monster = new Picture("Images/cthulhu.jpg", 5, "Cthyulu");
+var dragon = new Picture("Images/dragon.jpg", 6, "Dragon Meat");
+var pen = new Picture("Images/pen.jpg", 7, "Pen");
+var scissors = new Picture("Images/scissors.jpg", 8, "Scissors");
+var shark = new Picture("Images/shark.jpg", 9, "Shark Bag");
+var babysweep = new Picture("Images/sweep.jpg", 10, "Baby Sweeper");
+var usb = new Picture("Images/usb.jpg", 11, "USB Tail");
+var watercan = new Picture("Images/water_can.jpg", 12, "Water Can");
+var wineglass = new Picture("Images/wine_glass.jpg", 13, "Wine Glass");
+var unicorn = new Picture("Images/unicorn.jpg", 14, "Unicorn Meat");
 
 var picArray = [r2bag, banana, boots, chair, monster, dragon, pen, scissors, shark, babysweep, usb, watercan, wineglass, unicorn];
+
+var chart = null;
+
+function chartRender() {
+  chart = new CanvasJS.Chart("chartContainer", {
+    title: {text: "Picture Votes Counted"},
+    data: [
+      {
+        type: "column",
+        dataPoints: picArray
+      }
+    ]
+  });
+
+  chart.render();
+}
 
 var last3shown = []; //Will keep track of shown pictures.
 
@@ -81,14 +98,7 @@ function displaySwitch() {
     iniShow[is].style.display = "none";
   }
 
-  var figImages = document.getElementsByClassName('finalImage');
-  var figCaptions = document.getElementsByClassName('finalCap');
-  for (fi=0; fi<picArray.length; fi++) {
-    figImages[fi].src = picArray[fi].picSource;
-    figCaptions[fi].innerHTML = picArray[fi].voteCount+" Votes";
-  }
-
-  document.getElementById('hiddenSection').style.display = "inline-block";
+  document.getElementById('chartContainer').style.display = "block";
 
   document.getElementById('busPic').style.width = "13.3%";
   document.getElementById('header').style.marginBottom = "10px";
@@ -99,7 +109,7 @@ function clickPhoto() {
     for (objecti=0; objecti<picArray.length; objecti++) {
       var picObject = picArray[objecti];
       if (picObject.picNum == this.id) {
-          picObject.voteCount++
+          picObject.y++
       }
     }
     totalVoteCount++
@@ -107,7 +117,10 @@ function clickPhoto() {
     document.getElementById('votePlace').innerHTML = totalVoteCount;
     randomPicturePicker();
   }
-  else {displaySwitch();} //Calls display switch on photo vote 15.
+  else { //Calls display switch on photo vote 15.
+    chartRender();
+    displaySwitch();
+  }
 }
 
 
